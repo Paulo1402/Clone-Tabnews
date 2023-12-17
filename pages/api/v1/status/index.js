ad724802d5eca4176fb46a397e6b6c6505dd595b
Paulo1402 <paulobenatto02@gmail.com>
2023-12-17 19:28:03 -0300

    add `database.js`and setup database service

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database"
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 AS SUM;')
   response.status(200).json({ status: 200})
 }
 
